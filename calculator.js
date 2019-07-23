c282b0552b4e5e593135aa52738ba3eb4ace7bbd
Morgen Sudyanto <moondemon68@gmail.com>
2019-07-23 12:48:14 +0700

    fix int -> float

--- calculator.js
+++ calculator.js
@@ -30,8 +30,8 @@ app.get("/bmicalculator",function(req,res) {
 });
 
 app.post("/bmicalculator",function(req,res) {
-    var height=Number(req.body.height);
-    var weight=Number(req.body.weight);
+    var height=parseFloat(req.body.height);
+    var weight=parseFloat(req.body.weight);
     var result=weight/(height*height);
     res.send("Your BMI is " + result);
 });
